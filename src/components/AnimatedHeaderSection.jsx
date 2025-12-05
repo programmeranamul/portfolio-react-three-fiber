dd71d569d8c42c290eff2ac648d82c0b27de21cc
programmeranamul <programmer.anamul@gmail.com>
2025-12-05 14:39:36 +0600

    Hero Text Animation Complete

--- src/components/AnimatedHeaderSection.jsx
+++ src/components/AnimatedHeaderSection.jsx
@@ -1,5 +1,7 @@
 import React, { useRef } from "react";
 import AnimatedTextLines from "./AnimatedTextLines";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 function AnimatedHeaderSection({
   subTitle,
@@ -13,6 +15,31 @@ function AnimatedHeaderSection({
 
   const shouldSplitTitle = title.includes(" ");
   const titleParts = shouldSplitTitle ? title.split(" ") : [title];
+
+  useGSAP(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: withScrollTrigger
+        ? { trigger: contextRef.current }
+        : undefined,
+    });
+
+    tl.from(contextRef.current, {
+      y: "50vh",
+      duration: 1,
+      ease: "circ.out",
+    });
+
+    tl.from(
+      headerRef.current,
+      {
+        opacity: 0,
+        y: 200,
+        duration: 1,
+        ease: "circ.out",
+      },
+      "<+0.2"
+    );
+  }, []);
 
   return (
     <div ref={contextRef}>
--- src/components/AnimatedTextLines.jsx
+++ src/components/AnimatedTextLines.jsx
@@ -1,9 +1,27 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import React, { useRef } from "react";
 
 function AnimatedTextLines({ text, className }) {
   const containerRef = useRef(null);
   const lineRefs = useRef([]);
   const lines = text.split("\n").filter((line) => line.trim() !== "");
+
+  useGSAP(() => {
+    if (lineRefs.current.length > 0) {
+      gsap.from(lineRefs.current, {
+        y: 100,
+        opacity: 0,
+        duration: 1,
+        stagger: 0.3,
+        ease: "back.out",
+        scrollTrigger: {
+          trigger: containerRef.current,
+        },
+      });
+    }
+  }, []);
+
   return (
     <div ref={containerRef} className={className}>
       {lines.map((line, index) => (
