bfc43dc3c6e50a08499602acf12471d9362ba088
programmeranamul <programmer.anamul@gmail.com>
2025-12-05 15:02:34 +0600

    ed model added on hore section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import AnimatedHeaderSection from "./AnimatedHeaderSection";
+import { Canvas } from "@react-three/fiber";
+import { Planet } from "./Planet";
+import { Environment, Float, Lightformer } from "@react-three/drei";
 
 function Hero() {
+
+  // const isMobile = useMediaQuery({ maxWidth: 853 });
+
   const text = `I help growing brands and startups gain an
 unfair advantage through premium
 results driven webs/apps`;
@@ -9,10 +15,51 @@ results driven webs/apps`;
     <section id="home" className="flex flex-col justify-end min-h-screen">
       <AnimatedHeaderSection
         subTitle={"404 No Bugs Found"}
-        title={"Ali Sanati"}
+        title={"Anamul"}
         text={text}
         textColor={"text-black"}
       />
+
+      <figure
+        className="absolute inset-0 -z-50"
+        style={{ width: "100vw", height: "100vh" }}
+      >
+        <Canvas camera={{ position: [0, 0, -10], fov: 17.5, near: 1, far: 20 }}>
+          <ambientLight intensity={0.5} />
+          
+          <Float speed={0.5}>
+            <Planet />
+          </Float>
+          <Environment resolution={256}>
+            <group rotation={[-Math.PI / 3, 4, 1]}>
+              <Lightformer
+                form={"circle"}
+                intensity={2}
+                position={[0, 5, -9]}
+                scale={10}
+              />
+              <Lightformer
+                form={"circle"}
+                intensity={2}
+                position={[0, 3, 1]}
+                scale={10}
+              />
+              <Lightformer
+                form={"circle"}
+                intensity={2}
+                position={[-5, -1, -1]}
+                scale={10}
+              />
+              <Lightformer
+                form={"circle"}
+                intensity={2}
+                position={[10, 1, 0]}
+                scale={16}
+              />
+            </group>
+          </Environment>
+        </Canvas>
+      </figure>
     </section>
   );
 }
