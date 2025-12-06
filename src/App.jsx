b44dc414bdbdf528634f3810a7e9465e734b651c
programmeranamul <programmer.anamul@gmail.com>
2025-12-06 13:47:56 +0600

    service summary section complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-
-
-import './App.css'
-import Hero from './components/Hero'
-import Navbar from './components/Navbar'
+import "./App.css";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import ServiceSummary from "./components/ServiceSummary";
 
 function App() {
-
   return (
     <main className="relative w-screen min-h-screen overflow-x-auto">
-     <Navbar />
-     <Hero />
+      <Navbar />
+      <Hero />
+      <ServiceSummary />
+      <div className="min-h-screen"></div>
+      <div className="min-h-screen"></div>
+      <div className="min-h-screen"></div>
+      <div className="min-h-screen"></div>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
